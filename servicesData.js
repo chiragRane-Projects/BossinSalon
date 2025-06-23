8e94db3a2ee8dee2c530f87513a79b5fb764f749
Chirag Rane <beingchirag7@gmail.com>
2025-06-24 00:39:26 +0530

    landing page

---
+++ servicesData.js
@@ -0,0 +1,192 @@
+export const servicesData = [
+    {
+      category: "Hair Services",
+      subcategories: [
+        {
+          name: "Hair Wash",
+          gender: "Unisex",
+          services: [
+            { name: "Pineapple Wash", price: 220 },
+            { name: "Bamboo Wash", price: 250 },
+            { name: "Shine Shampoo Wash", price: 260 },
+            { name: "Dandruff Wash", price: 350 },
+            { name: "Hair Growth Wash", price: 370 },
+            { name: "L’ORÉAL Shampoo Wash", price: 400 },
+            { name: "Schwarzkopf Wash", price: 450 },
+            { name: "Keratine Wash", price: 470 },
+            { name: "BOTOX Wash", price: 500 },
+            { name: "Morrocan Wash", price: 600 },
+            { name: "Argan Wash", price: 700 },
+            { name: "Kerastase Wash", price: 1000 },
+          ],
+        },
+        {
+          name: "Relaxing Hair Spa",
+          gender: "Unisex",
+          services: [
+            { name: "Deep Conditioning", price: 400 },
+            { name: "Detox Spa", price: 600 },
+            { name: "BOSSIN Shine Spa", price: 650 },
+            { name: "L’Oréal Regular Spa", price: 700 },
+            { name: "L’Oréal Nourishing Spa", price: 1000 },
+            { name: "BOSSIN SHINE-Protein Spa", price: 1000 },
+            { name: "L’Oréal Protein Spa", price: 1500 },
+            { name: "Karatine Spa", price: 1700 },
+            { name: "BOTOX Spa", price: 2000 },
+            { name: "Argan Cocktail Glossing Spa", price: 2500 },
+            { name: "Schwarzkopf Fiber Spa", price: 3000 },
+            { name: "Morrocan Repair Spa", price: 4000 },
+            { name: "Morrocan Hydration Spa", price: 4500 },
+            { name: "Kerastase", price: 5500 },
+          ],
+        },
+      ],
+    },
+    {
+      category: "Hair Cutting & Shaving",
+      subcategories: [
+        {
+          name: "Haircuts",
+          gender: "Male",
+          services: [
+            { name: "Shape-up (Any Style)", price: 200 },
+            { name: "Hair Cut (Boy)", price: 150 },
+            { name: "Senior Citizen Cut", price: 100 },
+            { name: "Hair Set", price: 150 },
+          ],
+        },
+        {
+          name: "Haircuts",
+          gender: "Female",
+          services: [
+            { name: "Hair Cut (Girl)", price: 350 },
+            { name: "Clean Line (Straight Cut)", price: 200 },
+            { name: "U Turn (U Cut)", price: 200 },
+            { name: "Forward Graduation (Layered Cut)", price: 500 },
+            { name: "Reverse Graduation (Step Cut)", price: 500 },
+            { name: "Bangs", price: 150 },
+            { name: "Fringes", price: 150 },
+            { name: "Flicks", price: 150 },
+            { name: "Veg Cut", price: 500 },
+            { name: "Diana Cut", price: 500 },
+            { name: "Curtain Bangs", price: 200 },
+            { name: "Bob Cut", price: 700 },
+            { name: "Lob Cut", price: 700 },
+            { name: "Blunt Cut", price: 700 },
+            { name: "Wolf Cut", price: 800 },
+            { name: "Butterfly Cut", price: 1000 },
+            { name: "Designer Cut", price: 1200 },
+          ],
+        },
+        {
+          name: "Shaving",
+          gender: "Male",
+          services: [
+            { name: "Standard Shave", price: 150 },
+            { name: "Coco Butter Luxury Shave", price: 300 },
+            { name: "Royal Shave", price: 400 },
+            { name: "Beard Trim", price: 120 },
+            { name: "Beard Style", price: 100 },
+            { name: "Moustache Trim", price: 100 },
+          ],
+        },
+      ],
+    },
+    {
+      category: "Temporary Hair Styling",
+      subcategories: [
+        {
+          name: "Blow Dry",
+          gender: "Unisex",
+          services: [
+            { name: "Blast Dry", price: 250 },
+            { name: "Up to Shoulder", price: 350 },
+            { name: "Below Shoulder", price: 400 },
+            { name: "Mid Waist", price: 500 },
+            { name: "Below Waist", price: 600 },
+          ],
+        },
+        {
+          name: "Standard Ironing",
+          gender: "Unisex",
+          services: [
+            { name: "Up to Shoulder", price: 400 },
+            { name: "Below Shoulder", price: 500 },
+            { name: "Mid Waist", price: 700 },
+            { name: "Below Waist", price: 900 },
+            { name: "Add Steam-Pod", price: 100 },
+          ],
+        },
+      ],
+    },
+    {
+      category: "Skin Services",
+      subcategories: [
+        {
+          name: "Dtan Services",
+          gender: "Unisex",
+          services: [
+            { name: "Full Face (Ragaa)", price: 500 },
+            { name: "Full Face (Ozone)", price: 600 },
+            { name: "Full Face (Kanpeki)", price: 700 },
+            { name: "Full Face (O3 Whitening Brightening)", price: 1000 },
+            { name: "Neck", price: 200 },
+            { name: "Hands", price: 1000 },
+            { name: "Half Legs", price: 1200 },
+            { name: "Full Legs", price: 2000 },
+            { name: "Full Body", price: 4000 },
+          ],
+        },
+      ],
+    },
+    {
+      category: "Facial Services",
+      subcategories: [
+        {
+          name: "Clean-Ups",
+          gender: "Unisex",
+          services: [
+            { name: "Hydra Fresh", price: 600 },
+            { name: "Deep Pore", price: 700 },
+            { name: "Tann Clear", price: 800 },
+            { name: "Fruitsu", price: 900 },
+            { name: "Organic", price: 1000 },
+            { name: "Ainoha", price: 1500 },
+          ],
+        },
+      ],
+    },
+    {
+      category: "Manicure & Pedicure",
+      subcategories: [
+        {
+          name: "Manicure",
+          gender: "Unisex",
+          services: [
+            { name: "Standard", price: 500 },
+            { name: "Classic", price: 600 },
+            { name: "Kanpeki Pedi Pie", price: 700 },
+            { name: "Exotic Bomb", price: 800 },
+            { name: "D-Tan", price: 900 },
+            { name: "American Divine Choco", price: 1000 },
+          ],
+        },
+      ],
+    },
+    {
+      category: "Waxing",
+      subcategories: [
+        {
+          name: "Standard Flavored Wax",
+          gender: "Unisex",
+          services: [
+            { name: "Full Arm", price: 400 },
+            { name: "Half Arm", price: 150 },
+            { name: "Full Leg", price: 600 },
+            { name: "Half Leg", price: 300 },
+          ],
+        },
+      ],
+    },
+  ];
+  
